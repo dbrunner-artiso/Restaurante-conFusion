4f58cc8bcdd1823a6a3be476b05ff8de3fc7531d
Daniel Brunner <dbrunner@artiso.com>
2022-11-16 11:37:45 +0100

    End week 2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
+import { Leader } from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
+import { Location } from '@angular/common';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -7,9 +12,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  leaders: Array<Leader>;
 
-  ngOnInit() {
+  constructor(private leaderService: LeaderService, private location: Location) { }
+
+  ngOnInit(): void {
+    this.leaders = this.leaderService.getLeaders();
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
